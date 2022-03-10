cc2a56444e98d1fdc2fb0b6c3f422153a9ceb0b0
ucurtmaavcısı <simsekmerve1717@gmail.com>
2022-03-10 21:07:07 +0300

    çalışma

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 
+// const seats=document.getElementsByClassName("seat");
+// console.log(seats);
+// let selectAll=document.querySelector("#movie");
+// console.log(selectAll.textContent);
+
+// var counter=document.querySelector("#count");
+// console.log(counter);
+
+// var films=document.querySelector("#film");
+// console.log(films);
+
+
+
+// [...seats].forEach((element)=>{
+//     element.addEventListener("click",(e)=>{
+//         if(!e.target.classList.contains("occupied")){
+//            e.target.style.backgroundColor="#6feaf6" 
+//            counter.textContent++;
+//         }
+//     })
+// })
+
+
+// selectAll.addEventListener("change",(e)=>{
+//        films.innerText=selectAll.childElement()
+// })
+
+
+
+
