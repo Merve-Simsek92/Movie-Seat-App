40dc1406da66fcb8d05453df1d5d9a61c9819676
ucurtmaavcısı <simsekmerve1717@gmail.com>
2022-03-14 14:40:24 +0300

    çalışma

--- script.js
+++ script.js
@@ -1,45 +1,62 @@
-const container = document.querySelector(".container");
+const container=document.querySelector(".container");
 const allSeats=document.querySelectorAll(".container .seat");
-const notOccupiedsSeats=document.querySelector(".container .seat:not(.occupied)");
+const notOccupiedSeats=document.querySelector(".container .seat:not(.occupied)");
 const count=document.getElementById("count");
 const film=document.getElementById("film");
 const total=document.getElementById("total");
+
 const movieSelectBox=document.getElementById("movie");
+console.log(movieSelectBox);
+// let currentTicketPrice=localStorage.getItem("selectedMoviePrice") ? localStorage.getItem("selectedMoviePrice") : movieSelectBox.options[movieSelectBox.selectedIndex].value;
 
-let currentTicketPrice=localStorage.getItem("selectedMoviePrice") ? localStorage.getItem("selectedMoviePrice") : movieSelectBox.value;
+let currentMovieIndex=localStorage.getItem("selectedMovieIndex") ? localStorage.getItem("selectedMovieIndex") : movieSelectBox.selectedIndex;
 
-//movieIndex
-let currentMovieındex=localStorage.getItem("selectedMovieIndex") ? localStorage.getItem("selectedMovieIndex") : movieSelectBox.selectedIndex;
-
-
-
-
+window.onload=()=>{
+    
+    movieSelectBox.selectedIndex=currentMovieIndex;
+    displayUI();
+    updateMovieInfo();
+}
 movieSelectBox.addEventListener("change",(e)=>{
     let ticketPrice=e.target.value;
+    console.log(e.target);
     let movieIndex=e.target.selectedIndex;
     console.log(movieIndex);
-    setMovieDataToLocalStroge(ticketPrice,movieIndex);
+    updateMovieInfo();
+    setMovieDataLocalStorage(ticketPrice,movieIndex);
 })
-const  setMovieDataToLocalStroge=(movieIndex,ticketPrice)=>{
+const  setMovieDataLocalStorage =(ticketPrice,movieIndex)=>{
     localStorage.setItem("selectedMovieIndex",movieIndex);
-    localStorage.setItem("selectedMoviePrice",ticketPrice);
+    localStorage.setItem("selectedMoviePrice",ticketPrice)
 }
-
 container.addEventListener("click",(e)=>{
-  console.log(e.target);
-  if(e.target.classList.contains("seat") && !e.target.classList.contains("occupied")){
-      e.target.classList.toggle("selected")
-  }
-  updateMovieInfo();
-});
+    console.log(e.target);
+if(e.target.classList.contains("seat") && !e.target.classList.contains("occupied")){
+    e.target.classList.toggle("selected");
+updateMovieInfo()
+}
+})
+
+
 const updateMovieInfo=()=>{
     let selectedSeats=document.querySelectorAll(".row .seat.selected");
     let selectedSeatsIndexArray=[...selectedSeats].map(seat=> [...allSeats].indexOf(seat));
-    console.log(selectedSeatsIndexArray);
-    localStorage.setItem("selectedSeats",JSON.stringify(selectedSeatsIndexArray))
-
+    localStorage.setItem("selectedSeats",JSON.stringify(selectedSeatsIndexArray));
     count.innerText=selectedSeatsIndexArray.length;
-    total.innerText=selectedSeatsIndexArray.length * currentTicketPrice;
-    
+    total.innerText=selectedSeatsIndexArray.length*movieSelectBox.value;
+    film.innerText=movieSelectBox.options[movieSelectBox.selectedIndex].innerText.split("(")[0];
 
+
+}
+const displayUI=()=>{
+    let selectedSeatsFromStorage=JSON.parse(localStorage.getItem("selectedSeats"));
+    console.log(selectedSeatsFromStorage);
+    if(selectedSeatsFromStorage !== null && selectedSeatsFromStorage.length>0){
+allSeats.forEach((seat,index) =>{
+            if(selectedSeatsFromStorage.indexOf(index)>-1){
+                seat.classList.add("selected");
+
+            }
+        })
+    }
 }
